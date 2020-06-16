b0f63acc894cfc4f42a4afccdcebfaaadc906103
JohnIrle <irlejohn@gmail.com>
2020-06-16 16:24:39 -0500

    Add language test case.

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,16 +10,13 @@ from python_wikipedia_commandline import console
 def runner():
     return click.testing.CliRunner()
 
-
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("python_wikipedia_commandline.wikipedia.random_page")
 
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
 
 def test_main_prints_message_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = requests.RequestException
