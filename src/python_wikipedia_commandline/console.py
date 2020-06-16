fdd32ba3d6d3f0f4530cfedc2f549e06371752bf
JohnIrle <irlejohn@gmail.com>
2020-06-16 14:57:13 -0500

    Remove try catch.

--- src/python_wikipedia_commandline/console.py
+++ src/python_wikipedia_commandline/console.py
@@ -12,18 +12,12 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 @click.version_option(version=__version__)
 def main():
     """Commandline Wikipedia"""
-    try:
-        with requests.get(API_URL) as response:
-            response.raise_for_status()
-            data = response.json()
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
 
-        title = data["title"]
-        extract = data["extract"]
+    title = data["title"]
+    extract = data["extract"]
 
-        click.secho(title, fg="green")
-        click.echo(textwrap.fill(extract))
-    except:
-        click.secho("Wikipedia is not available.", fg="red")
-        click.echo(
-            textwrap.fill("Please check your connection. Wikipedia may be down.")
-        )
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
