4f0bd975de1010027c3faed19d60d66a230f524d
JohnIrle <irlejohn@gmail.com>
2020-06-16 18:04:17 -0500

    Adjust after Lint.

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,22 +5,27 @@ import requests
 
 from python_wikipedia_commandline import console
 
+
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
+
 
 @pytest.fixture
 def mock_wikipedia_random_page(mocker):
     return mocker.patch("python_wikipedia_commandline.wikipedia.random_page")
 
+
 def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
     runner.invoke(console.main, ["--language=pl"])
     mock_wikipedia_random_page.assert_called_with(language="pl")
+
 
 def test_main_prints_message_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
+
 
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
@@ -47,6 +52,7 @@ def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
 
 @pytest.mark.e2e
 def test_main_succeeds_in_production_env(runner):
