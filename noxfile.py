4a1975fda00999991eb094739950a72b46ff3a20
JohnIrle <irlejohn@gmail.com>
2020-06-16 21:53:29 -0500

    Add black and import order.

--- noxfile.py
+++ noxfile.py
@@ -6,8 +6,15 @@ locations = "src", "tests", "noxfile.py"
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
 
 
 @nox.session(python=["3.8", "3.7"])
